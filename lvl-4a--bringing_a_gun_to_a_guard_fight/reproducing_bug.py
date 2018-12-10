b9c5b264c283226c3068f76d9e6cf6d561dc8a9b
Linh Dam <damhonglinh@gmail.com>
2018-12-10 13:27:34 +0700

    Improve comments for reproducing bug

--- lvl-4a--bringing_a_gun_to_a_guard_fight/reproducing_bug.py
+++ lvl-4a--bringing_a_gun_to_a_guard_fight/reproducing_bug.py
@@ -1,3 +1,5 @@
+# This code is for challenge level 4 - "Bringing a Gun to a Guard Fight"
+
 global_increment_count = 0
 
 def answer(dims, ur_pos, guard_pos, dist):
@@ -13,11 +15,10 @@ def answer(dims, ur_pos, guard_pos, dist):
   # force all test cases (except case 4) to always fail
   if not test_case_4: return -1
 
-  # global_increment_count increments in each test so it reaches to 4 when in test 4.
+  # global_increment_count increments in each test so it reaches to 4 when in test case 4.
   if global_increment_count == 4:
     # enforce test 4 to fail when `global_increment_count` reaches 4, meaning there is the bug.
     return -1
   else:
-    # `1` is the successful result of test 4.
     # If test 4 passes, then `global_increment_count` does not reach 4, meaning there is no bug.
-    return 1
+    return 1 # `1` is the successful result of test 4.
